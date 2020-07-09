279352bb3daa4975ffeaf99f6024874f7a92b666
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-07-09 14:25:10 -0300

    aula GQL

--- GraphQl/original/projeto/index.js
+++ GraphQl/original/projeto/index.js
@@ -1,5 +1,32 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+const perfis = [
+    { id: 1, nome: 'comum' },
+    { id: 2, nome: 'administrador' }
+]
+
+const usuarios =[{
+    id: 1,
+    nome: 'João Silva',
+    email: 'joão@email.com',
+    idade: 19,
+    perfil_id: 1,
+},{
+    id: 2,
+    nome: 'Maria Gomes',
+    email: 'maria@email.com',
+    idade: 31,
+    perfil_id: 2,
+},{
+    id: 3,
+    nome: 'Ana Braga',
+    email: 'ana@mail.com',
+    idade: 26,
+    perfil_id: 1,
+}
+]
+
+
 const typeDefs = gql`
     scalar Date
 
@@ -10,6 +37,19 @@ const typeDefs = gql`
         idade: Int
         salario: Float
         vip: Boolean
+        perfil: Perfil
+    }
+
+    type Perfil {
+        id: Int
+        nome: String
+    }
+
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
     }
 
 
@@ -18,15 +58,36 @@ const typeDefs = gql`
         ola: String
         horaCerta: String
         usuarioLogado: Usuario
+        produtoEmDestaque: Produto
+        numerosMegaSena: [Int!]!
+        usuarios: [Usuario]
+        usuario(id: ID): Usuario
+        perfis: [Perfil]
+        perfil(id: Int): Perfil
     }
 `
 
 const resolvers = {
-    // Usuario: {
-    //     salario(usuario) {
-    //         return usuario.salario_real
-    //     }
-    // },
+    Produto: {
+        precoComDesconto(produto) {
+            if (produto.desconto){
+            return produto.preco *(1 - produto.desconto)
+        } else {
+            return produto.preco
+        }
+        }
+    },
+
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        },
+        
+        perfil(usuario) {
+            const sels = perfis.filter(p => p.id === usuario.perfil_id)
+            return sels ? sels[0] : null
+        }
+    },
     Query: {
         ola(){
             return 'Retornando uma string'
@@ -45,6 +106,49 @@ const resolvers = {
 
 
             }
+        },
+
+        produtoEmDestaque() {
+            return {
+                nome: 'Celular',
+                preco: 100.00,
+                desconto: 0.3,
+
+            }
+        },
+
+        numerosMegaSena() {
+            // return [4, 8, 13, 27, 33, 54]
+            const crescente = (a, b) => a - b
+            return Array(6)
+                .fill(0)
+                .map(n => parseInt(Math.random() * 60 + 1))
+                .sort(crescente)
+
+
+        },
+
+        usuarios() {
+            return usuarios
+        },
+
+        usuario(_, args) {
+            const selecionados = usuarios.filter(u => u.id == args.id)
+            return selecionados ? selecionados[0] : null
+        },
+        //  OU
+        // usuario(_, { id }) {
+        //     const selecionados = usuarios.filter(u => u.id == id)
+        //     return selecionados ? selecionados[0] : null
+        // },
+
+        perfis() {
+            return perfis
+        },
+
+        perfil(_, { id }) {
+            const sels = perfis.filter(p => p.id === id)
+            return sels ? sels[0] : null
         }
     }
 }
