de2e4cb222899d85224f6d23944d3f71187eb33d
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-07-09 16:34:12 -0300

    update de pastas

--- GraphQl/schema-query/projeto/data/db.js
+++ GraphQl/schema-query/projeto/data/db.js
@@ -9,18 +9,21 @@ const usuarios =[{
     email: 'joão@email.com',
     idade: 19,
     perfil_id: 1,
+    status: 'ATIVO',
 },{
     id: 2,
     nome: 'Maria Gomes',
     email: 'maria@email.com',
     idade: 31,
     perfil_id: 2,
+    status: 'INATIVO',
 },{
     id: 3,
     nome: 'Ana Braga',
     email: 'ana@mail.com',
     idade: 26,
     perfil_id: 1,
+    status: 'BLOQUEADO',
 }
 ]
 
