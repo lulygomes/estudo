39cb28757c5294ee1e1112232a68aa7e483d9f6b
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-07-10 16:22:30 -0300

    update

---
+++ GraphQl/mutations/resolvers/Mutation.js
@@ -0,0 +1,21 @@
+const { usuarios, proximoId } = require('../data/db')
+
+module.exports = {
+    // { nome, email, idade}
+    novoUsuario(_, args) {
+        const emailExistente = usuarios.some(u => u.email === args.email)
+        if (emailExistente) {
+            throw new Error('E-mail cadastrado')
+        }
+        const novo = {
+            id: proximoId,
+            ...args,
+            perfil_id: 1,
+            status: 'ATIVO'
+        }
+
+        usuarios.push(novo)
+        return novo
+        
+    }
+}
